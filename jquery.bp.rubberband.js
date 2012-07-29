7e10f9e3ab43924e45a00d253bfc552da27aa508
Ben Plum <benjaminplum@gmail.com>
2012-07-29 10:10:40 -0400

    Switched to Google Closure minifyer; removed debug code.

--- jquery.bp.rubberband.js
+++ jquery.bp.rubberband.js
@@ -26,7 +26,6 @@
 			methods._respond();
 		},
 		_respond: function(e) {
-			console.log(window.innerWidth);
 			methods._clearTimeout();
 			timeout = setTimeout(function() {
 				methods._doRespond();
@@ -34,17 +33,17 @@
 		},
 		_doRespond: function() {
 			methods._clearTimeout();
-			var w = (window.innerWidth) ? window.innerWidth : document.body.clientWidth - 20;
+			var width = (window.innerWidth) ? window.innerWidth : document.body.clientWidth - 20;
 			for (var point in settings.breakpoints) {
 				point = parseInt(point, 10);
 				if (settings.breakpoints[point + 1]) {
-					if (w <= settings.breakpoints[point] && w >= settings.breakpoints[point + 1]) {
+					if (width <= settings.breakpoints[point] && width >= settings.breakpoints[point + 1]) {
 						$('body').addClass('bp-min-' + settings.breakpoints[point + 1] + "-max-" + settings.breakpoints[point]);
 					} else {
 						$('body').removeClass('bp-min-' + settings.breakpoints[point + 1] + "-max-" + settings.breakpoints[point]);
 					}
 				}
-				if (w <= settings.breakpoints[point] && (w > settings.breakpoints[point + 1] || typeof settings.breakpoints[point + 1] === "undefined")) {
+				if (width <= settings.breakpoints[point] && (width > settings.breakpoints[point + 1] || typeof settings.breakpoints[point + 1] === "undefined")) {
 					if (!$('body').hasClass('bp-max-' + settings.breakpoints[point])) {
 						$('body').addClass('bp-max-' + settings.breakpoints[point]).addClass('bp-min-' + settings.breakpoints[point + 1]);
 						$('body').removeClass('bp-max-' + lastPoint.max).removeClass('bp-min-' + lastPoint.min);
@@ -75,7 +74,7 @@
 		} else if (typeof method === 'object' || !method) {
 			return methods.init.apply(this, arguments);
 		} else {
-			$.error("RESPONDER: '" +  method + "' does not exist.");
+			$.error("RUBBERBAND: '" +  method + "' does not exist.");
 		} 
 	};
 })(jQuery);
