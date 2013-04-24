827c0071e71f2f5e8260af4f325a5d2d2234b3f8
Ben Plum <mr@benplum.com>
2013-04-24 11:28:41 -0400

    Added vertical breakpoint support; Reworked options and event systems; Stills fire enter/exits on max sizes.

--- jquery.bp.rubberband.js
+++ jquery.bp.rubberband.js
@@ -1,7 +1,7 @@
 /*
  * Rubberband - Responsive breakpoint events
  * @author Ben Plum
- * @version 1.4.2
+ * @version 1.4.4
  *
  * Copyright © 2013 Ben Plum <mr@benplum.com>
  * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
@@ -12,13 +12,17 @@ if (jQuery) (function($) {
 	// Default options
 	var options = {
 		addClasses: false,
-		breakpoints: [1240, 980, 740, 500, 340],
+		breakpoints: {
+			horizontal: [1240, 980, 740, 500, 340],
+			vertical: []
+		},
 		debounce: 5
 	};
 	
 	// Helper vars
-	var lastPoint = {};
-	var timeout = null;
+	var lastPointVertical = {},
+		lastPointHorizontal = {},
+		timeout = null;
 	
 	// Public Methods
 	var pub = {};
@@ -28,8 +32,12 @@ if (jQuery) (function($) {
 		options = jQuery.extend(options, opts);
 		
 		// Add widest point
-		options.breakpoints.push(Infinity);
-		options.breakpoints.sort(_sort);
+		options.breakpoints.horizontal.push(Infinity);
+		options.breakpoints.horizontal.sort(_sort);
+		
+		// Add tallest point
+		options.breakpoints.vertical.push(Infinity);
+		options.breakpoints.vertical.sort(_sort);
 		
 		// Bind events
 		$(window).resize(_respond);
@@ -48,42 +56,88 @@ if (jQuery) (function($) {
 	function _doRespond() {
 		_clearTimeout();
 		
-		// Normalize window width
-		var width = (window.innerWidth) ? window.innerWidth : document.body.clientWidth - 20;
+		// Normalize window width & height
+		var width = (window.innerWidth) ? window.innerWidth : document.body.clientWidth - 20,
+			height = (window.innerHeight) ? window.innerHeight : document.body.clientHeight - 20;
 		
-		// Loop through breakpoints
-		for (var point in options.breakpoints) {
+		var dataExit = {},
+			dataEnter = {};
+		
+		// Loop through breakpoints - Horizontal
+		for (var point in options.breakpoints.horizontal) {
 			point = parseInt(point, 10);
 			
 			// Add classes to body - psuedo breakpoints for IE
-			if (options.breakpoints[point + 1] && options.addClasses) {
-				if (width <= options.breakpoints[point] && width >= options.breakpoints[point + 1]) {
-					$("body").addClass("bp-min-" + options.breakpoints[point + 1] + "-max-" + options.breakpoints[point]);
+			if (options.breakpoints.horizontal[point + 1] && options.addClasses) {
+				if (width <= options.breakpoints.horizontal[point] && width >= options.breakpoints.horizontal[point + 1]) {
+					$("body").addClass("bp-min-width-" + options.breakpoints.horizontal[point + 1] + "-max-width-" + options.breakpoints.horizontal[point]);
 				} else {
-					$("body").removeClass("bp-min-" + options.breakpoints[point + 1] + "-max-" + options.breakpoints[point]);
+					$("body").removeClass("bp-min-width-" + options.breakpoints.horizontal[point + 1] + "-max-width-" + options.breakpoints.horizontal[point]);
 				}
 			}
 			
 			// Check if we"re in a new breakpoint
-			if (width <= options.breakpoints[point] && (width > options.breakpoints[point + 1] || typeof options.breakpoints[point + 1] === "undefined") && lastPoint.max !== options.breakpoints[point]) {
+			if (width <= options.breakpoints.horizontal[point] && (width > options.breakpoints.horizontal[point + 1] || typeof options.breakpoints.horizontal[point + 1] === "undefined") && lastPointHorizontal.max !== options.breakpoints.horizontal[point]) {
 				// Add classes to body - psuedo breakpoints for IE
-				if (options.addClasses && (!$("body").hasClass("bp-max-" + options.breakpoints[point]))) {
-					$("body").addClass("bp-max-" + options.breakpoints[point])
-							 .addClass("bp-min-" + options.breakpoints[point + 1])
-							 .removeClass("bp-max-" + lastPoint.max)
-							 .removeClass("bp-min-" + lastPoint.min);
+				if (options.addClasses && (!$("body").hasClass("bp-max-width-" + options.breakpoints.horizontal[point]))) {
+					$("body").addClass("bp-max-width-" + options.breakpoints.horizontal[point])
+							 .addClass("bp-min-width-" + options.breakpoints.horizontal[point + 1])
+							 .removeClass("bp-max-width-" + lastPointHorizontal.max)
+							 .removeClass("bp-min-width-" + lastPointHorizontal.min);
 				}
 				
 				// Fire events!
-				if (typeof lastPoint.max != "undefined") {
-					$(window).trigger("rubberband.exit", [ lastPoint.max ]);
+				if (typeof lastPointHorizontal.max != "undefined") {
+					dataExit.horizontal = lastPointHorizontal.max;
 				}
-				$(window).trigger("rubberband.enter", [ options.breakpoints[point] ]);
+				dataEnter.horizontal = options.breakpoints.horizontal[point];
 				
 				// Update current breakpoint
-				lastPoint.max = options.breakpoints[point];
-				lastPoint.min = options.breakpoints[point + 1];
+				lastPointHorizontal.max = options.breakpoints.horizontal[point];
+				lastPointHorizontal.min = options.breakpoints.horizontal[point + 1];
 			}
+		}
+		
+		// Loop through breakpoints - Vertical
+		for (var point in options.breakpoints.vertical) {
+			point = parseInt(point, 10);
+			
+			// Add classes to body - psuedo breakpoints for IE
+			if (options.breakpoints.vertical[point + 1] && options.addClasses) {
+				if (height <= options.breakpoints.vertical[point] && height >= options.breakpoints.vertical[point + 1]) {
+					$("body").addClass("bp-min-height-" + options.breakpoints.vertical[point + 1] + "-max-height-" + options.breakpoints.vertical[point]);
+				} else {
+					$("body").removeClass("bp-min-height-" + options.breakpoints.vertical[point + 1] + "-max-height-" + options.breakpoints.vertical[point]);
+				}
+			}
+			
+			// Check if we"re in a new breakpoint
+			if (height <= options.breakpoints.vertical[point] && (height > options.breakpoints.vertical[point + 1] || typeof options.breakpoints.vertical[point + 1] === "undefined") && lastPointVertical.max !== options.breakpoints.vertical[point]) {
+				// Add classes to body - psuedo breakpoints for IE
+				if (options.addClasses && (!$("body").hasClass("bp-max-height-" + options.breakpoints.vertical[point]))) {
+					$("body").addClass("bp-max-height-" + options.breakpoints.vertical[point])
+							 .addClass("bp-min-height-" + options.breakpoints.vertical[point + 1])
+							 .removeClass("bp-max-height-" + lastPointVertical.max)
+							 .removeClass("bp-min-height-" + lastPointVertical.min);
+				}
+				
+				// Fire events!
+				if (typeof lastPointVertical.max != "undefined") {
+					dataExit.vertical = lastPointVertical.max;
+				}
+				dataEnter.vertical = options.breakpoints.vertical[point];
+				
+				// Update current breakpoint
+				lastPointVertical.max = options.breakpoints.vertical[point];
+				lastPointVertical.min = options.breakpoints.vertical[point + 1];
+			}
+		}
+		
+		if (dataEnter.vertical || dataEnter.horizontal) {
+			$(window).trigger("rubberband.enter", [ dataEnter ]);
+		}
+		if (dataExit.vertical || dataExit.horizontal) {
+			$(window).trigger("rubberband.exit", [ dataExit ]);
 		}
 	}
 	
