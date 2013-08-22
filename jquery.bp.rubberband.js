9bd2ed76c32e955f79a75a1bc1bb4ee66bd20791
Alastair Brayne <al@perchten.co.uk>
2013-08-22 23:37:04 +0100

    Added a promise for when the plugin has loaded

--- jquery.bp.rubberband.js
+++ jquery.bp.rubberband.js
@@ -10,71 +10,72 @@
 if (jQuery) (function($) {
 
     // Default options
-    var supported = (window.matchMedia !== undefined);
-        options = {
-            debounce: 5,
-            minWidth: [0],
-            maxWidth: [Infinity],
-            minHeight: [0],
-            maxHeight: [Infinity],
-            unit: "px"
-        };
+	var supported = (window.matchMedia !== undefined);
+		options = {
+			debounce: 5,
+			minWidth: [0],
+			maxWidth: [Infinity],
+			minHeight: [0],
+			maxHeight: [Infinity],
+			unit: "px"
+		};
 
-    // Helper vars
-    var mqMatches = {},
-        mqStrings = {
-            minWidth: "min-width",
-            maxWidth: "max-width",
-            minHeight: "min-height",
-            maxHeight: "max-height"
-        },
-        timeout = null;
+	// Helper vars
+	var mqMatches = {},
+		mqStrings = {
+			minWidth: "min-width",
+			maxWidth: "max-width",
+			minHeight: "min-height",
+			maxHeight: "max-height"
+		},
+		timeout = null;
 
     var state = undefined;
+    var loaded = new $.Deferred();
 
-    // Public Methods
-    var pub = {};
+	// Public Methods
+	var pub = {};
 
-    // Initialize
-    function _init(opts) {
-        opts = opts || {};
-        // Extend!
-        for (var i in mqStrings) {
-            options[i] = (opts[i]) ? $.merge(opts[i], options[i]) : options[i];
-        }
-        options = $.extend(options, opts);
+	// Initialize
+	function _init(opts) {
+		opts = opts || {};
+		// Extend!
+		for (var i in mqStrings) {
+			options[i] = (opts[i]) ? $.merge(opts[i], options[i]) : options[i];
+		}
+		options = $.extend(options, opts);
 
-        options.minWidth.sort(_sortD);
-        options.maxWidth.sort(_sortA);
-        options.minHeight.sort(_sortD);
-        options.maxHeight.sort(_sortA);
+		options.minWidth.sort(_sortD);
+		options.maxWidth.sort(_sortA);
+		options.minHeight.sort(_sortD);
+		options.maxHeight.sort(_sortA);
 
-        // Bind events to specific media query events
-        for (var i in mqStrings) {
-            mqMatches[i] = {};
-            for (var j in options[i]) {
-                var _mq = window.matchMedia( "(" + mqStrings[i] + ": " + (options[i][j] == Infinity ? 100000 : options[i][j]) + options.unit + ")" );
-                _mq.addListener(_respond);
-                mqMatches[i][ options[i][j] ] = _mq;
-            }
-        }
+		// Bind events to specific media query events
+		for (var i in mqStrings) {
+			mqMatches[i] = {};
+			for (var j in options[i]) {
+				var _mq = window.matchMedia( "(" + mqStrings[i] + ": " + (options[i][j] == Infinity ? 100000 : options[i][j]) + options.unit + ")" );
+				_mq.addListener(_respond);
+				mqMatches[i][ options[i][j] ] = _mq;
+			}
+		}
 
-        // Fire initial event
-        _respond();
-    }
+		// Fire initial event
+		_respond();
+	}
 
-    function _respond() {
-        _clearTimeout();
-        timeout = setTimeout(function() {
-            _doRespond()
-        }, options.debouce);
-    }
+	function _respond() {
+		_clearTimeout();
+		timeout = setTimeout(function() {
+			_doRespond()
+		}, options.debouce);
+	}
 
-    function _doRespond() {
-        console.log("start");
+	function _doRespond() {
         _setState();
-        $(window).trigger("snap", [ this.state ]);
-    }
+		$(window).trigger("snap", [ this.state ]);
+        this.loaded.resolve();
+	}
 
 
     function _setState() {
@@ -98,32 +99,32 @@ if (jQuery) (function($) {
         return this.state;
     }
 
-    // Sort array
-    function _sortA(a, b) {
-        return (b - a);
-    }
-    function _sortD(a, b) {
-        return (a - b);
-    }
+	// Sort array
+	function _sortA(a, b) {
+		return (b - a);
+	}
+	function _sortD(a, b) {
+		return (a - b);
+	}
 
-    // Clear debouncer
-    function _clearTimeout() {
-        if (timeout != null) {
-            clearTimeout(timeout);
-            timeout = null;
-        }
-    }
+	// Clear debouncer
+	function _clearTimeout() {
+		if (timeout != null) {
+			clearTimeout(timeout);
+			timeout = null;
+		}
+	}
 
-    // Define plugin
-    $.rubberband = function(method) {
-        // Check for matchMedia() support
-        if (supported) {
-            if (pub[method]) {
-                return pub[method].apply(this, Array.prototype.slice.call(arguments, 1));
-            } else if (typeof method === "object" || !method) {
-                return _init.apply(this, arguments);
-            }
-        }
-        return this;
-    };
+	// Define plugin
+	$.rubberband = function(method) {
+		// Check for matchMedia() support
+		if (supported) {
+			if (pub[method]) {
+				return pub[method].apply(this, Array.prototype.slice.call(arguments, 1));
+			} else if (typeof method === "object" || !method) {
+				return _init.apply(this, arguments);
+			}
+		}
+		return this;
+	};
 })(jQuery);
